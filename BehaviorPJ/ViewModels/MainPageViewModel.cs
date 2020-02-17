b4cd1757381e17192fd304e11f69b177537694d8
放浪 軍師 <roamschemer@gmail.com>
2020-02-17 10:39:20 +0900

    ページ遷移修正

--- BehaviorPJ/ViewModels/MainPageViewModel.cs
+++ BehaviorPJ/ViewModels/MainPageViewModel.cs
@@ -8,21 +8,14 @@ using System.Reactive.Linq;
 namespace BehaviorPJ.ViewModels {
     public class MainPageViewModel : ViewModelBase {
         public ReadOnlyReactiveCollection<PageData> PageDatas { get; }
-        public ReactiveProperty<PageData> CollectionSelectedItem { get; } = new ReactiveProperty<PageData>();
-        public AsyncReactiveCommand<PageData> ListTapped { get;} = new AsyncReactiveCommand<PageData>();
+        public AsyncReactiveCommand<PageData> ListTapped { get;} 
         public MainPageViewModel(INavigationService navigationService, PageModel pageModel) : base(navigationService) {
             PageDatas = pageModel.Pages.ToReadOnlyReactiveCollection();
-            //CollectionSelectedItem.Where(x => x != null).Subscribe(async (x) => {
-            //    var p = new NavigationParameters { { "PageData", x } };
-            //    await navigationService.NavigateAsync("Page1", p);
-            //    CollectionSelectedItem.Value = null; //選択を消す
-            //});
-            ListTapped.Subscribe(async (x) => {
-                if (x == null) return;
-                var p = new NavigationParameters { { "PageData", x } };
-                await navigationService.NavigateAsync("Page1", p);
-                CollectionSelectedItem.Value = null; //選択を消す
-            });
+            ListTapped = new AsyncReactiveCommand<PageData>()
+                .WithSubscribe(async (x) => {
+                    var p = new NavigationParameters { { "PageData", x } };
+                    await navigationService.NavigateAsync("Page1", p);
+                });
         }
     }
 }
